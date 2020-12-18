b4c492be11b61df4c2eb9f23eabe1b087827d2dd
senhordrio <adriosilvao@gmail.com>
2020-12-17 21:35:31 -0300

    adicao de gets para os campos de beneficiario

--- sistema.inss/beneficiario/Beneficiario.java
+++ sistema.inss/beneficiario/Beneficiario.java
@@ -4,15 +4,23 @@ public abstract class Beneficiario {
     private String cpf;
     private String nome;
 
-    public Beneficiario(String cpf, String nome){
+    protected Beneficiario(String cpf, String nome){
         this.cpf = cpf;
         this.nome = nome;
     }
 
     public abstract String calcularAposentadoria();
 
+    public String getCpf(){
+      return this.cpf;
+    }
+
+    public String getNome(){
+      return this.nome;
+    }
+
     @Override
     public String toString(){
-      return "Nome: " + this.nome + "\n" + "Cpf: " + this.cpf + "\n" + "Aposentadoria: " + calcularAposentadoria();
+      return "Nome: " + getNome() + "\n" + "Cpf: " + getCpf() + "\n" + "Aposentadoria: " + calcularAposentadoria();
     }
 }
